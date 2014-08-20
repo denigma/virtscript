b7eb7f9ba1b283792f8b91ea0be205181df68be7
Anton Kulaga <antonkulaga@gmail.com>
2014-08-20 12:41:47 +0300

    file generation

--- index.py
+++ index.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+from jinja2 import Environment, FileSystemLoader
+
 from wtforms import Form, BooleanField, IntegerField, StringField, PasswordField, validators
+from wtforms.validators import DataRequired
 
 class ContainerForm(Form):
-    id = IntegerField('CTID') # VEID контейнера
-    name = StringField("CTNAME")   # IP адрес контейнера
-    ip = StringField("CTIP")  # hostname контейнера
-    banned = StringField("BANNED") # IP адрес контейнера
-    allow2ct = StringField("10.0.5.18")# Разрешаем соединения между DMZ контейнером и этими подсетями
-    verelay = StringField("VERELAY")# Можно ли контейнеру спамить?
-    DNAT4PORTS_TCP = StringField("DNAT4PORTS_TCP")  # Можно ли контейнеру спамить?
-    DNAT2PORTS_TCP= StringField("DNAT2PORTS_TCP") # Пробрасываем на указанный порт контейнера внешний порт
-    DNAT4PORTS_UDP = StringField("DNAT4PORTS_UDP") # Пробрасываем указанный внешний порт на контейнер
-    DNAT2PORTS_UDP=StringField("DNAT2PORTS_UDP") # Пробрасываем на указанный порт контейнера внешний порт
-    DMZSTATUS = StringField(" DMZSTATUS")  # В DMZ ли данный контейнер?
+    id = IntegerField('CTID', default=101, validators=[DataRequired()]) # VEID контейнера
+    name = StringField("CTNAME", default="semantic", validators=[DataRequired()])   # IP адрес контейнера
+    ip = StringField("CTIP", default="192.168.1.104", validators=[DataRequired()])  # hostname контейнера
+    banned = StringField("BANNED", default="") # IP адрес контейнера
+    allow2ct = StringField("ALLOW2CT", default="")# Разрешаем соединения между DMZ контейнером и этими подсетями
+    verelay = StringField("VERELAY", default="yes")# Можно ли контейнеру спамить?
+    DNAT4PORTS_TCP = StringField("DNAT4PORTS_TCP", default="4569 9000 7070 5000")   # Пробрасываем указанный внешний порт на контейнер
+    DNAT2PORTS_TCP= StringField("DNAT2PORTS_TCP", default="22 80 7070 5000") # Пробрасываем на указанный порт контейнера внешний порт
+    DNAT4PORTS_UDP = StringField("DNAT4PORTS_UDP", default="") # Пробрасываем указанный внешний порт на контейнер
+    DNAT2PORTS_UDP=StringField("DNAT2PORTS_UDP", default="") # Пробрасываем на указанный порт контейнера внешний порт
+    DMZSTATUS = StringField(" DMZSTATUS", default="no")  # В DMZ ли данный контейнер?
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -28,12 +31,19 @@ def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
 
+def save(form,path = "./firewall/"):
+    env = Environment(loader=FileSystemLoader('templates'))
+    filename = path+form.name.data+".dmz.inc"
+    template = env.get_template('file_to_save.txt')
+    f = open(filename,'w')
+    f.write(template.render(form=form))
+    f.close()
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = ContainerForm(request.form)
-    print(form)
+    save(form)
     return render_template('ports.html', form=form, result = "<h1>SUCCESS</h1>")
 
 if __name__ == "__main__":
